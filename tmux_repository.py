bc183b232060aff06d5e761e392bc3e90ba77094
Martin Kolman <martin.kolman@gmail.com>
2014-08-16 15:16:39 +0200

    Add a tmux wrapper for the repository update

---
+++ tmux_repository.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# a wrapper that runs the repository update inside a tmux session
+# * the summary log goes to the main tab
+# * the other tabs have the individual pool logs
+
+import os
+import time
+import sys
+import subprocess
+
+log_folder = "repo_update_logs_%s" % time.strftime("%Y.%m.%d-%H:%M:%S")
+print("log folder: %s" % log_folder)
+
+#cmd = "tmux -u -f /usr/share/anaconda/tmux.conf start"
+
+os.environ['REPO_ARGV'] = " ".join(sys.argv[1:])
+os.environ['LOGS'] = log_folder
+print("starting tmux wrapper")
+subprocess.check_call(['tmux', '-u', '-f', 'core/data/tmux.conf', 'start'],
+                      env=os.environ)
+subprocess.check_call(['tmux', 'attach'],
+                      env=os.environ)
+print("tmux wrapping done")
