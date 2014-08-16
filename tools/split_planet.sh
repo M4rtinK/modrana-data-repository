b5eeddab6aeb727bd5f15f5163f48744958b97fd
Martin Kolman <martin.kolman@gmail.com>
2014-08-16 20:04:21 +0200

    fix calling of osmosis

--- tools/split_planet.sh
+++ tools/split_planet.sh
@@ -6,9 +6,12 @@ then
 else
     OSMOSIS=$OSMOSIS_PATH
 fi
+echo "path to osmosis:"
+echo $OSMOSIS_PATH
+
 PLANET=../planet/planet-latest.osm.pbf
 POLYS=../tools/polys/
 OUT="compress=none planet/split/"
 BUFFER="--buffer bufferCapacity=100000"
 
-./$OSMOSIS --read-pbf-fast $PLANET --tee 8 ${BUFFER} --bp file=${POLYS}africa.poly ${BUFFER} --write-pbf ${OUT}africa.osm.pbf ${BUFFER} --bp file=${POLYS}antarctica.poly ${BUFFER} --write-pbf ${OUT}antarctica.osm.pbf ${BUFFER} --bp file=${POLYS}asia.poly ${BUFFER} --write-pbf ${OUT}asia.osm.pbf ${BUFFER} --bp file=${POLYS}australia-oceania.poly ${BUFFER} --write-pbf ${OUT}australia-oceania.osm.pbf ${BUFFER} --bp file=${POLYS}central-america.poly ${BUFFER} --write-pbf ${OUT}central-america.osm.pbf ${BUFFER} --bp file=${POLYS}europe.poly ${BUFFER} --write-pbf ${OUT}europe.osm.pbf ${BUFFER} --bp file=${POLYS}north-america.poly ${BUFFER} --write-pbf ${OUT}north-america.osm.pbf ${BUFFER} --bp file=${POLYS}south-america.poly ${BUFFER} --write-pbf ${OUT}south-america.osm.pbf
+sh $OSMOSIS --read-pbf-fast $PLANET --tee 8 ${BUFFER} --bp file=${POLYS}africa.poly ${BUFFER} --write-pbf ${OUT}africa.osm.pbf ${BUFFER} --bp file=${POLYS}antarctica.poly ${BUFFER} --write-pbf ${OUT}antarctica.osm.pbf ${BUFFER} --bp file=${POLYS}asia.poly ${BUFFER} --write-pbf ${OUT}asia.osm.pbf ${BUFFER} --bp file=${POLYS}australia-oceania.poly ${BUFFER} --write-pbf ${OUT}australia-oceania.osm.pbf ${BUFFER} --bp file=${POLYS}central-america.poly ${BUFFER} --write-pbf ${OUT}central-america.osm.pbf ${BUFFER} --bp file=${POLYS}europe.poly ${BUFFER} --write-pbf ${OUT}europe.osm.pbf ${BUFFER} --bp file=${POLYS}north-america.poly ${BUFFER} --write-pbf ${OUT}north-america.osm.pbf ${BUFFER} --bp file=${POLYS}south-america.poly ${BUFFER} --write-pbf ${OUT}south-america.osm.pbf
