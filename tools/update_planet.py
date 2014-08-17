63cca808e3a1588ee4a2aa721aeb5451df8d34ea
Martin Kolman <martin.kolman@gmail.com>
2014-08-17 14:26:28 +0200

    Fix planet filename

--- tools/update_planet.py
+++ tools/update_planet.py
@@ -5,7 +5,7 @@ import hashlib
 
 PLANET_FOLDER = "planet"
 PLANET_FOLDER_PATH = os.path.abspath(PLANET_FOLDER)
-PLANET_FILENAME = "planet-latest.pbf"
+PLANET_FILENAME = "planet-latest.osm.pbf"
 PLANET_PATH = os.path.abspath(os.path.join(PLANET_FOLDER, PLANET_FILENAME))
 # located in Europe, good speed, updated weekly
 PLANET_URL = "http://ftp5.gwdg.de/pub/misc/openstreetmap/planet.openstreetmap.org/pbf/planet-latest.osm.pbf"
