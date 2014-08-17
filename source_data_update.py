811f2fc313eabe2996228cda00ec01c3c5aebb05
Martin Kolman <martin.kolman@gmail.com>
2014-08-17 12:34:47 +0200

    Log source data update to a per-run folder

--- source_data_update.py
+++ source_data_update.py
@@ -1,52 +1,41 @@
 #!/usr/bin/python
 import os
 import time
-import datetime
 
-def prettyTimeDiff(dtSeconds):
-    """
-    Get a datetime object or a int() Epoch timestamp and return a
-    pretty string like 'an hour ago', 'Yesterday', '3 months ago',
-    'just now', etc
-    """
-    diff = datetime.timedelta(seconds=dtSeconds)
-    second_diff = diff.seconds
-    day_diff = diff.days
+from core.utils import prettyTimeDiff, createFolderPath
 
-    if day_diff < 0:
-        return ''
+log_folder = "logs/source_data_update_logs_%s" % time.strftime("%Y.%m.%d-%H:%M:%S")
+log_folder = os.path.abspath(log_folder)
 
-    if day_diff == 0:
-        if second_diff < 60:
-            return str(second_diff) + " seconds"
-        if second_diff < 3600:
-            return str(second_diff / 60) + " minutes"
-        if second_diff < 86400:
-            return str(second_diff / 3600) + " hours"
-    if day_diff < 7:
-        return str(day_diff) + " days"
-    if day_diff < 31:
-        return str(day_diff / 7) + " weeks"
-    if day_diff < 365:
-        return str(day_diff / 30) + " months"
-    return str(day_diff / 365) + " years"
+# as the source data update can happen independently on the repository update
+# it has it's own log folder - it should be easy to correlate which source data
+# update was followed by which repository update by the date
+
+if not createFolderPath(log_folder):
+    print("ERROR: can't create log folder for the source data update run in:")
+    print(log_folder)
+    print("log data for this update run might not be gathered or the update run might fail outright")
+
+planet_update_log = os.path.join(log_folder, "update_planet.log")
+planet_split_log = os.path.join(log_folder, "split_planet.log")
+continents_split_log = os.path.join(log_folder, "split_continents.log")
 
 print("starting modRana repository source data update")
 start=time.time()
 print("updating the planet osm file")
-os.system("./tools/update_planet.sh")
+os.system("./tools/update_planet.sh&>%s" % planet_update_log)
 dt = int(time.time() - start)
 print("planet osm file update finished in %s" % prettyTimeDiff(dt))
 
 print("splitting the planet into continent sized chunks")
 start1=time.time()
-os.system("tools/split_planet.sh>split_planet.log")
+os.system("./tools/split_planet.sh&>%s" % planet_split_log)
 dt = int(time.time() - start1)
 print("planet splitting finished in %s" % prettyTimeDiff(dt))
 
 print("splitting the continents into regions")
 start2=time.time()
-os.system("tools/split_continents.py>split_continents.log")
+os.system("./tools/split_continents.py&>%s" % continents_split_log)
 dt = int(time.time() - start2)
 print("continent splitting finished in %s" % prettyTimeDiff(dt))
 
