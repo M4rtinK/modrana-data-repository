c5baec4c21d5467c9f9920198d1a9336cddb694a
Martin Kolman <martin.kolman@gmail.com>
2014-08-17 13:42:07 +0200

    Fix the planet update script to actually work in the future and add initial sanity checking

---
+++ tools/update_planet.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python
+import shutil
+import os
+import urllib
+import hashlib
+
+from core import utils
+
+PLANET_FOLDER = "planet"
+PLANET_FILENAME = "planet-latest.pbf"
+PLANET_PATH = os.path.join(PLANET_FOLDER, PLANET_FILENAME)
+# located in Europe, good speed, updated weekly
+PLANET_URL = "http://ftp5.gwdg.de/pub/misc/openstreetmap/planet.openstreetmap.org/pbf/planet-latest.osm.pbf"
+PLANET_MD5_URL = "http://ftp5.gwdg.de/pub/misc/openstreetmap/planet.openstreetmap.org/pbf/planet-latest.osm.pbf.md5"
+GiB = 1024 * 1024 * 1024
+PLANET_SANITY_THRESHOLD = 20 * GiB
+
+# osmupdate does transfer only the diffs if you provide it the right timestamp
+# but unfortunately takes too much time in the and
+# - we can download the whole file much faster
+# - maybe try it with uncompressed planet file ?
+#
+#echo "running osmupdate"
+#osmupdate -v planet-latest.pbf planet-latest-updated.pbf
+#echo "replacing old planet file"
+#mv planet-latest-updated.pbf planet-latest.pbf
+
+# remove the old planet file
+#os.remove(PLANET_PATH)
+# download the new one with wget
+#os.system("wget %s -p %s" % (PLANET_URL, PLANET_PATH))
+
+# some sanity checking
+
+print("= sanity checking the planet file")
+sane = True
+planet_size_bytes = 0
+try:
+    os.path.getsize(PLANET_PATH)
+except OSError:
+    planet_size_bytes = 0
+
+# size
+print("* planet size check")
+print("planet size: %s" % utils.bytes2PrettyUnitString(planet_size_bytes))
+print("planet size sanity threshold: %s" % utils.bytes2PrettyUnitString(PLANET_SANITY_THRESHOLD))
+if planet_size_bytes >= PLANET_SANITY_THRESHOLD:
+    print("* planet size: OK")
+else:
+    print("* planet size: NOK")
+    sane = False
+
+# md5
+print("* md5 check")
+remote_checksum = None
+local_checksum = None
+if PLANET_MD5_URL:
+    remote_checksum = None
+    try:
+        print("retrieving remote planet file checksum from:\n%s" % PLANET_MD5_URL)
+        remote = urllib.urlopen(PLANET_MD5_URL)
+        remote_checksum = remote.read().split(" ")[0]
+        print("remote checksum retrieved")
+    except Exception as e:
+        print("ERROR: retrieving remote md5 checksum failed")
+        print(e)
+
+    # it only makes sense to compute the local checksum if we have the remote checksum
+    if remote_checksum:
+        try:
+            print("computing local md5 checksum")
+            local_checksum = hashlib.md5(PLANET_PATH).hexdigest()
+            print("local md5 checksum dome")
+        except Exception as e:
+            print("local md5 checksum computation failed")
+            print(e)
+
+if remote_checksum is None or local_checksum is None:
+    print("skipping the md5 check - can't get remote or local checksum")
+else:
+    if remote_checksum == local_checksum:
+        print("* md5 checksum: OK")
+    else:
+        print("* md5 checksum: NOK")
+        sane = False
+
+if sane:
+    print("planet update finished successfully")
+    exit()
+else:
+    print("planet update failed")
+    exit(1)
